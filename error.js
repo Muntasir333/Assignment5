d033eda6011e31ca8816db76fc3814ea5af0d65e
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-03-09 22:03:27 +0600

    third Commit

--- error.js
+++ error.js
@@ -51,6 +51,77 @@ function filterIssues(){
 
 }
 
+const loadDetails= async(id)=>{
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    const data = await res.json();
+    displayDetails(data.data)
+
+}
+
+const displayDetails =(issue)=>{
+    const detailsBox = document.getElementById("details")
+    let priorityCol;
+    if(issue.priority=='high'){
+        priorityCol="bg-red-300";
+    }
+    else if (issue.priority == 'medium'){
+        priorityCol="bg-yellow-200";
+    }
+    else if (issue.priority=='low'){
+        priorityCol="bg-green-200";
+    }
+
+    let labelHTML = '';
+    for (let label of issue.labels){
+        let colorClass ='bg-gray-300';
+        icon ='';
+
+        if (label.toLowerCase()==='bug'){
+            colorClass= 'bg-red-300';
+            icon='<i class="fa-solid fa-bug"></i>'
+        }
+        else if (label.toLowerCase()==='help wanted'){
+             colorClass= 'bg-yellow-200';
+            icon='<i class="fa-regular fa-life-ring"></i>'
+        }
+        else if (label.toLowerCase()==='enhancement'){
+             colorClass= 'bg-green-200';
+            icon='<i class="fa-solid fa-wand-magic-sparkles"></i>'
+        }
+         else if (label.toLowerCase()==='good first issue'){
+             colorClass= 'bg-violet-200';
+            icon='<i class="fa-brands fa-jira"></i>'
+        }
+        else if (label.toLowerCase()==='documentation'){
+             colorClass= 'bg-blue-200';
+            icon='<i class="fa-solid fa-file-code"></i>'
+        }
+        labelHTML +=`<div class="${colorClass} px-2 py-1 text-[12px] rounded-xl inline-block mr-1">${icon} ${label}</div>`
+    }
+    detailsBox.innerHTML=` <div class="modal-box min-w-[700px] min-h-[415px]">
+    <div class=" bg-white p-8">
+    <h2 class="font-semibold text-2xl">${issue.title}</h2>
+    <div class="flex gap-3 items-center font-normal text-[12px] text-[#64748B] mt-2 mb-6">
+        <h1>Opened <h1><img src="./assets/Ellipse 5.png"></h1>
+        <h1>Opened by ${issue.assignee} <h1><img src="./assets/Ellipse 5.png"></h1>
+        <h1>${issue.updatedAt}</h1>
+    </div>
+    <div class="mb-6">${labelHTML}</div>
+    <p class="font-normal text-[16px] text-[#64748B] mb-6">${issue.description}</p>
+    <div class="grid grid-cols-2 items-center bg-[#F8FAFC] w-[636px] h-[81px] p-4">
+        <div>
+            <h1 class="font-normal text-[16px] text-[#64748B] ">Assignee:</h1>
+            <h2 class="text-xl font-bold">${issue.assignee}</h2>
+        </div>
+        <div>
+            <h1 class="font-normal text-[16px] text-[#64748B] ">Priority:</h1>
+            <h2 class="${priorityCol} w-20 py-1 inline-block text-center rounded-xl font-medium text-[16px]">${issue.priority}</h2>
+        </div>
+    </div>
+</div>`;
+    document.getElementById("my_modal_1").showModal();
+}
+
 
 
 
@@ -113,7 +184,7 @@ function display(issues){
         <h1 class="${priorityCol} w-20 py-1 inline-block text-center rounded-xl font-medium text-[12px]">${issue.priority}</h1>
     </div>
     <div>
-        <h2 class="font-semibold text-[14px]">${issue.title}</h2>
+        <h2 onclick="loadDetails(${issue.id})" class="font-semibold text-[14px]">${issue.title}</h2>
         <p class="font-normal text-[12px] text-[#64748B] truncate">${issue.description}</p>
     </div>
     <div class="flex gap-2">
